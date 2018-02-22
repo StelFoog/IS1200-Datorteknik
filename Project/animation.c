9e9a97992e462d7c5dad5807393abf17a2d92c02
Samuel <samlar@kth.se>
2018-02-22 11:28:13 +0100

    improved animation functions

--- Project/animation.c
+++ Project/animation.c
@@ -35,16 +35,55 @@ void idleAnimation(battlePokemon *pkmn1, battlePokemon *pkmn2) {
 }
 
 void attackAnimation(moveStruct *atk, battlePokemon *pkmn1, battlePokemon *pkmn2, char atkingPkmn) {
+    unsigned char timeoutcount = 0, moveCount = 0;
     if(((atk->phySpecPrio >> 4) & 3) == 2) {
-        drawSprite(96, 0, pkmn2->sprite->front, 32, 32);
-        drawSprite(0, 32, pkmn1->sprite->back, 32, 32);
+        while(timeoutcount < 10) {
+            clrScr();
+            if(IFS(0) & 0x100){         // check if interrupt flag is enabled
+                timeoutcount++;           // Increment timeoutcount
+                if(moveCount>4) {
+                    moveCount = 4;
+                } else {
+                    moveCount++;
+                }
+                IFSCLR(0) = 0x100;        //Reset the timeout flag
+            }
+            drawSprite(96, 0, pkmn2->sprite->front, 32, 32);
+            drawSprite(0, 32, pkmn1->sprite->back, 32, 32);
 
-        drawString("P1 HP: ", 52, 128-62);
-        hpString(str, pkmn1->hp, 0);
-        drawSprite(str, 52, 20);
+            drawString("P1 HP: ", 52, 128-62);
+            hpString(str, pkmn1->hp, 0);
+            drawSprite(str, 52, 20);
 
-        drawString("P2 HP: ", 2, 2);
-        hpString(str, pkmn2->hp, 0);
-        drawSprite(str, 2, 44);
+            drawString("P2 HP: ", 2, 2);
+            hpString(str, pkmn2->hp, 0);
+            drawSprite(str, 2, 44);
+
+            if(atkingPkmn == 1) {
+                drawSprite(statusMove, 8, 44-moveCount);
+            } else {
+                drawSprite(statusMove, 104, 12-moveCount);
+            }
+            update();
+        }
+    } else if(((atk->phySpecPrio >> 4) & 3) == 1) {
+        unsigned char timeoutcount = 0;
+        while(timeoutcount < 10) {
+            clrScr();
+            if(IFS(0) & 0x100){         // check if interrupt flag is enabled
+                timeoutcount++;           // Increment timeoutcount
+                IFSCLR(0) = 0x100;        //Reset the timeout flag
+            }
+            drawSprite(96, 0, pkmn2->sprite->front, 32, 32);
+            drawSprite(0, 32, pkmn1->sprite->back, 32, 32);
+
+            drawString("P1 HP: ", 52, 128-62);
+            hpString(str, pkmn1->hp, 0);
+            drawSprite(str, 52, 20);
+
+            drawString("P2 HP: ", 2, 2);
+            hpString(str, pkmn2->hp, 0);
+            drawSprite(str, 2, 44);
+        }
     }
 }
