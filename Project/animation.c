5a2740a9b10dbc0b933e90db47a0fcdc4c0cc5a0
Axel Pettersson <axel@pettersson.cc>
2018-03-05 09:31:15 +0100

    Fixed faint animation

--- Project/animation.c
+++ Project/animation.c
@@ -204,7 +204,7 @@ void faintAnimation(battlePokemon *pkmn1, battlePokemon *pkmn2, char faintedPkmn
             if(counter < 4) {
                 drawSprite(0, 32, pkmn1->sprite->back, 32, 32);
             } else if(counter < 12) {
-                drawSprite(96, 32 + counter-4, pkmn2->sprite->front, 32, 32);
+                drawSprite(0, 32 + counter-4, pkmn1->sprite->back, 32, 32);
             }
             drawSprite(96, moveCount, pkmn2->sprite->front, 32, 32);
         }
